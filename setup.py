6ce794cc0250f3a2b1977562ae914ce5bfddb13c
Peter Urda <peter.urda@gmail.com>
2016-11-19 08:18:43 -0800

    Add setup.py and it's MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,54 @@
+"""
+Copyright 2016 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-letsencrypt',
+    version='0.9.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache License, Version 2.0',
+    description="A Simple django app to handle Let's Encrypt ACME challenges.",
+    long_description=README,
+    url='https://github.com/urda/django-letsencrypt',
+    author='Peter Urda',
+    author_email='noreply@urda.cc',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
