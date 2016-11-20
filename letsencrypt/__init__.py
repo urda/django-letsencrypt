084b226efcf82f9b9190275bcdd30f78340b1785
Peter Urda <peter.urda@gmail.com>
2016-11-20 17:49:48 -0500

    Version bump

--- letsencrypt/__init__.py
+++ letsencrypt/__init__.py
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 default_app_config = 'letsencrypt.apps.LetsEncryptConfig'
