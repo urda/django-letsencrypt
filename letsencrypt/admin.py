52ab26f3842568822bc608edcc53b7b47d0358e6
Peter Urda <peter.urda@gmail.com>
2016-11-20 19:00:32 -0500

    Configure and expand admin page for the model

--- letsencrypt/admin.py
+++ letsencrypt/admin.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import AcmeChallenge
 
@@ -22,11 +23,18 @@ from .models import AcmeChallenge
 class AcmeChallengeAdmin(admin.ModelAdmin):
     """Admin options for the ACME Challenge"""
 
-    list_display = (
-        'id',
-        'challenge',
-        'response',
-    )
+    def format_acme_url(self, acme_object):
+        """Format the ACME url from the ACME challenge object"""
+        object_url = acme_object.get_acme_url()
+
+        if object_url:
+            return format_html(
+                "<a href='{}'>ACME Challenge Link</a>",
+                object_url,
+            )
+
+        return '-'
+    format_acme_url.short_description = 'Link'
 
     fieldsets = [
         ('ACME Request', {
@@ -38,16 +46,35 @@ class AcmeChallengeAdmin(admin.ModelAdmin):
         ('Metadata', {
             'fields': [
                 'id',
+                'format_acme_url',
                 'created_ts',
                 'updated_ts',
             ],
         }),
     ]
 
+    date_hierarchy = 'created_ts'
+
+    list_display = (
+        'challenge',
+        'format_acme_url',
+    )
+
+    list_filter = [
+        'created_ts',
+        'updated_ts',
+    ]
+
     readonly_fields = [
         'id',
         'created_ts',
         'updated_ts',
+        'format_acme_url',
+    ]
+
+    search_fields = [
+        'challenge',
+        'response',
     ]
 
 
