87af748f4b2470cf28f2fa3b1e51336961bbd127
Peter Urda <peter.urda@gmail.com>
2017-02-05 17:21:43 -0800

    Version bump

--- letsencrypt/__init__.py
+++ letsencrypt/__init__.py
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '1.0.7'
+__version__ = '2.0.0'
 
 default_app_config = 'letsencrypt.apps.LetsEncryptConfig'
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-letsencrypt',
-    version='1.0.7',
+    version='2.0.0',
     packages=['letsencrypt'],
     include_package_data=True,
     license='Apache License, Version 2.0',
