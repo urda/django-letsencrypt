54a702536878104baa8cdf49c0333ebb2fc57cb8
Peter Curet <petercuret@gmail.com>
2017-03-01 21:40:42 +0100

    Import cleanup

--- letsencrypt/views.py
+++ letsencrypt/views.py
@@ -15,8 +15,9 @@ limitations under the License.
 """
 
 
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
 from .models import AcmeChallenge
 
 
