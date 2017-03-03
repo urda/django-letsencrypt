7ac354ffd483b7bfd45c8601a6a8621d11978745
Peter Curet <petercuret@gmail.com>
2017-03-03 17:45:36 +0100

    whitespace removed

--- letsencrypt/views.py
+++ letsencrypt/views.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
