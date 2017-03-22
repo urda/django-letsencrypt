ccd0f9e6f98189bd20c0d682b5f2c12fea56a1c6
Peter Urda <peter.urda@gmail.com>
2017-03-21 20:32:49 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-letsencrypt',
-    version='1.1.1',
+    version='2.0.0',
     packages=['letsencrypt'],
     include_package_data=True,
     license='Apache License, Version 2.0',
