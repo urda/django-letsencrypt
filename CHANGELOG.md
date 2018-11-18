be9e286f2fb0dc5a6f918a1c8c4dfc7fcbc3b5f0
Peter Urda <peter.urda@gmail.com>
2018-11-18 11:55:38 -0800

    Version bump.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # django-letsencrypt CHANGELOG
 
+## v3.1.0
+
 ## v3.0.1
 
 - Breaking Changes
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-letsencrypt',
-    version='3.0.1',
+    version='3.1.0',
     python_requires=">=3.4, <4",
     packages=['letsencrypt'],
     include_package_data=True,
