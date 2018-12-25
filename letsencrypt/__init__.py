e0149163d3544cc0a8b4df64187237cdc75797ed
Peter Urda <peter.urda@gmail.com>
2018-12-25 02:39:36 -0500

    Adjust Copyright year on touched file for release.

--- letsencrypt/__init__.py
+++ letsencrypt/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2016-2017 Peter Urda
+Copyright 2016-2018 Peter Urda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
