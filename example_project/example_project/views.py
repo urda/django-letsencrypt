eb8c2d679ae210378717d7bdd003cecb4a1a8926
Peter Urda <peter.urda@gmail.com>
2020-10-31 03:38:47 -0700

    Updated README and CHANGELOG and copyright dates.

--- example_project/example_project/views.py
+++ example_project/example_project/views.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017 Peter Urda
+Copyright 2017-2020 Peter Urda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
