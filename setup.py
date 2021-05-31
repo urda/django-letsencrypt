51d4e3af675f8cc0a381aaf67c40282b4e104f01
Peter Urda <peter.urda@gmail.com>
2021-05-31 03:06:52 -0700

    Version bump ahead of changes

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-letsencrypt',
-    version='4.0.0',
+    version='4.0.1',
     python_requires=">=3.6, <4",
     packages=['letsencrypt'],
     include_package_data=True,
